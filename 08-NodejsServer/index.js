f9a63e1a2dc43d45be7d919d5cde426d089eec5c
MehmetTurkerTuncer-UIA <mehmettt@student.uia.no>
2025-08-04 21:26:56 +0200

    First commit

---
+++ 08-NodejsServer/index.js
@@ -0,0 +1,30 @@
+'use strict'
+
+/*...................*/
+
+//  NODEJS
+
+/*...................*/
+
+// HTTPSERVER
+
+const http = require('node:http') // BuiltIn Module
+
+/*...................*/
+
+// http.createServer((request, response)=> { } )
+
+const app = http.createServer((request, response)=> {
+        response.end('Hello World')
+        console.log('Console print')
+
+ } )
+
+
+ // Default server domain (local domain) = localhost
+ // Default server IP (local IP) = 127.0.0.1
+ app.listen(8000, ()=> console.log('Server started: http://127.0.0.1:8000'))
+
+
+/*...................*/
+/*...................*/
