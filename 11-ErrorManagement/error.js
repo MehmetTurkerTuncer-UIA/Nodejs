e1234f0d0c091881bfc41e92ffda45f2a2c745c3
MehmetTurkerTuncer-UIA <mehmettt@student.uia.no>
2025-08-06 20:51:04 +0200

    tryCatch

--- 11-ErrorManagement/error.js
+++ 11-ErrorManagement/error.js
@@ -10,17 +10,17 @@ require("dotenv").config();
 const PORT = process.env.PORT || 8000;
 
 /* -------------------------------------------------------*/ 
+/* ------------------------------------------------------- 
 
 app.get('/user/:id', function (req, res) {
 
-    /* ------------------------------------------------------- 
-
+    
     throw new Error('Hata olustu')
     res.send({
          id: req.params.id,
          message: 'Hello World'
      })
-         */
+         
 
     if(isNaN(req.params.id)){
 
@@ -30,10 +30,47 @@ app.get('/user/:id', function (req, res) {
 
     }
      
-    });
 
+    });
+    */
 
 /* ------------------------------------------------------- */
+
+app.get('/user/:id', function (req, res) {
+
+    try{
+        if(isNaN(req.params.id)){
+
+            throw new Error('Id parametresi sayi olmak zorundadir')
+        }else{
+            res.status(200).send(
+                {
+                    
+                    error: false,
+                    message: 'Hersey gicir'
+    
+                }
+            )
+        }
+        
+
+    }catch (err) {
+        // Hata olmasi halinde catch calisir
+        
+        res.status(500).send(
+            {
+                
+                error: true,
+                message: err.message
+
+            }
+        )
+    }
+  
+
+     
+    });
+
 
 
 
