56aa1103728c2cd9c83d75a3aec08224ef01e34f
MehmetTurkerTuncer-UIA <mehmettt@student.uia.no>
2025-08-07 13:30:20 +0200

    expressStatic

--- 11-ErrorManagement/builtins.js
+++ 11-ErrorManagement/builtins.js
@@ -12,14 +12,24 @@ const PORT = process.env.PORT || 8000;
 /* ------------------------------------------------------- */
 
 // Gelen data almak
+app.use(express.json())
+
+app.use(express.text())
+
+app.use(express.urlencoded({extended: true}))
 
 app.all('/', (req, res) => {
 
     res.send({
-        message: 'Tamamdir'
+        params:req.params,
+        query: req.query,
+        headers: req.headers,
+        body: req.body
     })
 })
 
+//Static dosyalara ulasabilmek icin app use da bu kodu kullanmak lazim
+app.use('/images', express.static('./images'))
 
 /* ------------------------------------------------------- */
 app.listen(PORT, () => console.log("Running: http://127.0.0.1:" + PORT));
