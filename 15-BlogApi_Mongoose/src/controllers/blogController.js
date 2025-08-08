dc04d0a1adf63a8ad6a4b3902ba4461c8fe6c0a1
MehmetTurkerTuncer-UIA <mehmettt@student.uia.no>
2025-08-08 22:40:21 +0200

    update

--- 15-BlogApi_Mongoose/src/controllers/blogController.js
+++ 15-BlogApi_Mongoose/src/controllers/blogController.js
@@ -3,3 +3,21 @@
     EXPRESSJS - BLOG Project with Mongoose
 ------------------------------------------------------- */
 
+const {BlogCategory, BlogPost} = require('../models/blogModel')
+
+
+module.exports.blogCategory = {
+
+    create: async ( req, res) =>{
+
+        
+
+    }
+    
+}
+
+module.exports.blogPost = {
+
+
+
+}
