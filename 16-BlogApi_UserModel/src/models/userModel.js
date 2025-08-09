18983da73896d29506f6e866ab188d912b58f2a0
MehmetTurkerTuncer-UIA <mehmettt@student.uia.no>
2025-08-09 21:04:07 +0200

    passwordCryptAndEmailValidation

--- 16-BlogApi_UserModel/src/models/userModel.js
+++ 16-BlogApi_UserModel/src/models/userModel.js
@@ -3,6 +3,27 @@
     EXPRESSJS - BLOG Project with Mongoose
 ------------------------------------------------------- */
 
+
+
+/*------------------------------------------------------- */
+
+// Password Encrypt (PBKDF2 Method):
+// https://nodejs.org/api/crypto.html#cryptopbkdf2syncpassword-salt-iterations-keylen-digest
+
+const crypto = require('node:crypto')
+
+const keyCode = process.env.SECRET_KEY  // sifreleme anahtari
+const loopCount = 10000 // dongu sayisi
+const charCount = 32  // 64 karakteloik geri donus icin 32 yaz
+const encType = 'sha512'  // sifreleme algoritmasi
+
+const passwordEncrypt = function (password) {
+
+    return crypto.pbkdf2Sync(password, keyCode, loopCount, charCount, encType).toString('hex')
+}
+
+/*------------------------------------------------------- */
+
 const mongoose = require('mongoose')
 
 const UserSchema = new mongoose.Schema({
@@ -10,14 +31,24 @@ const UserSchema = new mongoose.Schema({
         type: String,
         trim: true,
         unique: true,
-        required: true
+        required: true,
+        validate: () => {
+            if(email.includes('@') && email.includes('.')){
+                return true
+            }else{
+                return false
+            }
+        }
     },
 
     password: {
         
         type: String,
         trim: true,
-        required: true
+        required: true,
+        // set: passwordEncrypt
+        set: (password) => { 
+            return passwordEncrypt(password) } // veri kaydederken, return edilen data kaydedilir 
 
     },
 
