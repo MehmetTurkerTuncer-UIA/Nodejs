fbd8af40a3c8f7e0aab9493730857214d9bebc0f
MehmetTurkerTuncer-UIA <mehmettt@student.uia.no>
2025-08-15 16:23:46 +0200

    queryHandler

--- 18-BlogApi_Search_Sort_Pagination/src/controllers/blogController.js
+++ 18-BlogApi_Search_Sort_Pagination/src/controllers/blogController.js
@@ -75,55 +75,7 @@ module.exports.blogPost = {
   list: async (req, res) => {
     //console.log(req.query);
 
-    // FILTER //SEARCH   // SORDT  // PAGINATION
-
-    // FILTER
-    // URL?filter[fieldName]=value&filter[fieldName2]=value2
-    const filter = req.query?.filter || {};
-    //console.log(filter);
-
-    // SEARCH
-    // URL?search[fieldName]=value&search[fieldName2]=value2
-    const search = req.query?.search || {};
-    //console.log(search);
-    // https://www.mongodb.com/docs/manual/reference/operator/query/regex/
-
-    //console.log(key, search[key])
-    for (let key in search) search[key] = { $regex: search[key] };
-    //console.log(search);
-
-    //SORT
-    // URL?sort[fieldName]=аsc&sort[fieldName2]=desc
-    const sort = req.query?.sort || {};
-    //console.log(sort)
-
-    // PAGINATION
-
-    // URL?page=3&limit=15
-
-    let limit = Number(req.query?.limit);
-    limit = limit > 0 ? limit : Number(process.env?.PAGE_SIZE || 20);
-    console.log(limit);
-
-    // PAGE
-    let page = Number(req.query?.page);
-    page = page > 0 ? page : 1;
-
-    // SKIP
-    let skip = Number(req.query?.skip);
-    skip = skip > 0 ? skip : (page - 1) * limit;
-
-    //const data = await BlogPost.find()
-    //const data = await BlogPost.find({}, {categoryId: true, title: true, content: true})
-    //const data = await BlogPost.find({}, {categoryId: 1, title: 1, content: 1}).populate('categoryId')
-    //const data = await BlogPost.find().populate('categoryId')
-
-    const data = await BlogPost.find({ ...filter, ...search })
-      .sort(sort)
-      .skip(skip)
-      .limit(limit)
-      .populate('categoryId');
-
+    
     res.status(200).send({
       error: false,
       result: data,
